de8350802915a5c1278f750443195f621efabff7
bat-orshikh@letumongolia.com <bat-orshikh@letumongolia.com>
2021-05-05 05:06:42 +0000

    Created MainForm2; Edited settings; Deleted MainForm

--- client_code/MainForm2/__init__.py
+++ client_code/MainForm2/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import MainFormTemplate
+from ._anvil_designer import MainForm2Template
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -7,7 +7,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-class MainForm(MainFormTemplate):
+class MainForm2(MainForm2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
